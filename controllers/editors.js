1152c7886798fdb6dac868fca191553f4dda5571
Serene Ko <hazel.r.ko@gmail.com>
2026-03-11 01:59:48 -0400

    load editors tables

--- controllers/editors.js
+++ controllers/editors.js
@@ -1,9 +1,17 @@
 const express = require('express');
 var router = express.Router()
+const UsersModel = require('../models/users.js')
+const ArticlesModel = require('../models/articles.js')
 
 // Display the editors page
 router.get("/", async function(req, res)
 {
+  const users = await UsersModel.getAllUsers();
+  const articles = await ArticlesModel.getAllArticles();
+
+  req.TPL.users = users;
+  req.TPL.articles = articles;
+
   res.render("editors", req.TPL);
 });
 
